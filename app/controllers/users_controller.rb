77ccb84177b9eaf2236351f0699b2fc32f149785
andynarf <untalandy@gmail.com>
2019-11-07 16:41:52 -0300

    working on login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
-class UsersController < ApplicationController
+# frozen_string_literal: true
 
+class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
@@ -13,7 +14,7 @@ class UsersController < ApplicationController
     if @user.save
       # Handle a successful save.
       redirect_to @user
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = 'Welcome to the Sample App!'
     else
       render 'new'
     end
@@ -21,8 +22,8 @@ class UsersController < ApplicationController
 
   private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
 end
